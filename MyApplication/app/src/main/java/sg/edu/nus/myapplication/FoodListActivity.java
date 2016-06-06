7d67cda0118007aa3f4468a098fda4c873763274
yujulucy <yuju888@hotmail.com>
2016-06-06 18:28:15 +0800

    more changes

--- MyApplication/app/src/main/java/sg/edu/nus/myapplication/FoodListActivity.java
+++ MyApplication/app/src/main/java/sg/edu/nus/myapplication/FoodListActivity.java
@@ -2,29 +2,52 @@ package sg.edu.nus.myapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutCompat;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class FoodListActivity extends ListActivity {
+public class FoodListActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_food_list);
 
-        String[] food = {"Chicken Rice", "Char Siew Rice", "Chicken Teriyaki Don", "Nasi Lemak"};
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
-                android.R.layout.simple_list_item_1, food);
-        ListView listView = (ListView)findViewById(R.id.listView);
-        listView.setAdapter(adapter);﻿
+        populateListView();
+        registerClickCallback();
     }
 
-    @Override
+    private void populateListView() {
+        String[] food = {"Chicken Rice", "Char Siew Rice", "Chicken Teriyaki Don", "Nasi Lemak"};
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+                R.layout.activity_food_list, food);
+        ListView list = (ListView) findViewById(R.id.lvList);
+        //list.setAdapter(adapter);﻿
+    }
+
+    private void registerClickCallback() {
+        ListView list = (ListView) findViewById(R.id.lvList);
+        list.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View viewClicked, int position, long id) {
+                TextView textView = (TextView) viewClicked;
+                String msg = "You clicked # " + position + ", which is string: "
+                        + textView.getText().toString();
+                Toast.makeText(FoodListActivity.this, msg, Toast.LENGTH_LONG).show();
+            }
+        });
+    }
+
+    /*@Override
     public boolean onCreateOptionsMenu(Menu menu) {
         //Inflate the menu; this adds items to te action bar if it is present
-        getMenuInflater().inflate(R.menu.food_list, menu);
+        getMenuInflater().inflate(R.menu.menu_food_list, menu);
         return true;
     }
 
@@ -37,5 +60,5 @@ public class FoodListActivity extends ListActivity {
             return true;
         }
         return super.onOptionsItemSelected(item);
-    }
+    }*/
 }
--- MyApplication/app/src/main/java/sg/edu/nus/myapplication/ListActivity.java
+++ MyApplication/app/src/main/java/sg/edu/nus/myapplication/ListActivity.java
@@ -5,6 +5,8 @@ import android.support.v7.app.ActionBarActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import java.util.List;
 
@@ -18,17 +20,29 @@ public class ListActivity extends AppCompatActivity {
         DBHandler db = new DBHandler(this);
 
         //Inserting food/rows
-        Log.d("Insert: ", "Inserting ..");
+        /*Log.d("Insert: ", "Inserting ..");
         db.addFood(new Data(1, "Chicken Rice", 618, 26, 23, 76, 1314));
         db.addFood(new Data(2, "Char Siew Rice", 602, 25, 16, 90, 1391));
         db.addFood(new Data(3, "Chicken Teriyaki Don", 641, 34, 24, 73, 947));
-        db.addFood(new Data(4, "Nasi Lemak", 279, 12, 13, 29, 778));
+        db.addFood(new Data(4, "Nasi Lemak", 279, 12, 13, 29, 778));*/
 
         //Reading all foods
-        Log.d("Reading: ", "Reading all food..");
+        //Log.d("Reading: ", "Reading all food..");
         List<Data> foods = db.getAllFood();
 
-        for (Data food : foods) {
+        if(foods.size() != 0) {
+            ListView listView = (ListView) findViewById(R.id.lvFoodList);
+
+            String[] food = {"Chicken Rice", "Char Siew Rice", "Chicken Teriyaki Don", "Nasi Lemak"};
+
+            ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(
+                    this,
+                    android.R.layout.simple_list_item_1,
+                    food);
+            listView.setAdapter(arrayAdapter);
+        }
+
+        /*for (Data food : foods) {
             String log = "Id: " + food.getId() + " , Name: " + food.getName()
                     + " , Calories: " + food.getCalories() + " , Protein: " + food.getProtein()
                     + " , Fat: " + food.getFat() + " , Carbohydrates: " + food.getCarbo()
@@ -36,6 +50,6 @@ public class ListActivity extends AppCompatActivity {
 
         //Writing foods to log
             Log.d("Shop: ", log);
-        }
+        }*/
     }
 }
