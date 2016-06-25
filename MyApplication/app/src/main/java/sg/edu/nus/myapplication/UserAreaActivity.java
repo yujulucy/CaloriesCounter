ffab3517cd97cda517ea725cc5ec94bdae957a5f
yujulucy <yuju888@hotmail.com>
2016-06-26 01:54:57 +0800

    image buttons and UI

--- MyApplication/app/src/main/java/sg/edu/nus/myapplication/UserAreaActivity.java
+++ MyApplication/app/src/main/java/sg/edu/nus/myapplication/UserAreaActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.TextView;
 
 public class UserAreaActivity extends AppCompatActivity {
@@ -17,6 +18,11 @@ public class UserAreaActivity extends AppCompatActivity {
         final TextView tvAccount = (TextView) findViewById(R.id.tvAccount);
         final TextView tvMessage = (TextView) findViewById(R.id.tvMessage);
         final Button bLogout = (Button) findViewById(R.id.bLogout);
+
+        final ImageButton ibList = (ImageButton) findViewById(R.id.ibList);
+        final ImageButton ibBookmark = (ImageButton) findViewById(R.id.ibBookmark);
+        final ImageButton ibCalc = (ImageButton) findViewById(R.id.ibCalc);
+        final ImageButton ibPlus = (ImageButton) findViewById(R.id.ibPlus);
 
         Intent intent = getIntent();
         String name = intent.getStringExtra("name");
@@ -32,6 +38,30 @@ public class UserAreaActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 Intent registerIntent = new Intent(UserAreaActivity.this, LoginActivity.class);
+                UserAreaActivity.this.startActivity(registerIntent);
+            }
+        });
+
+        ibList.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent registerIntent = new Intent(UserAreaActivity.this, ListActivity.class);
+                UserAreaActivity.this.startActivity(registerIntent);
+            }
+        });
+
+        ibCalc.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent registerIntent = new Intent(UserAreaActivity.this, Calculator.class);
+                UserAreaActivity.this.startActivity(registerIntent);
+            }
+        });
+
+        ibPlus.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent registerIntent = new Intent(UserAreaActivity.this, NewFoodActivity.class);
                 UserAreaActivity.this.startActivity(registerIntent);
             }
         });
