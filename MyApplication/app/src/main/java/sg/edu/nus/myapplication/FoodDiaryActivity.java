4a603116f4e2f01d6b74bdb5127007508b1d7419
yujulucy <yuju888@hotmail.com>
2016-08-23 13:57:05 +0800

    add food buttons

--- MyApplication/app/src/main/java/sg/edu/nus/myapplication/FoodDiaryActivity.java
+++ MyApplication/app/src/main/java/sg/edu/nus/myapplication/FoodDiaryActivity.java
@@ -16,8 +16,35 @@ public class FoodDiaryActivity extends Activity {
         setContentView(R.layout.activity_food_diary);
 
         final Button bBreakfast = (Button) findViewById(R.id.bBreakfast);
+        final Button bLunch = (Button) findViewById(R.id.bLunch);
+        final Button bDinner = (Button) findViewById(R.id.bDinner);
+        final Button bSnack = (Button) findViewById(R.id.bSnack);
 
         bBreakfast.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent registerIntent = new Intent(FoodDiaryActivity.this, AddFoodDataActivity.class);
+                FoodDiaryActivity.this.startActivity(registerIntent);
+            }
+        });
+
+        bLunch.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent registerIntent = new Intent(FoodDiaryActivity.this, AddFoodDataActivity.class);
+                FoodDiaryActivity.this.startActivity(registerIntent);
+            }
+        });
+
+        bDinner.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent registerIntent = new Intent(FoodDiaryActivity.this, AddFoodDataActivity.class);
+                FoodDiaryActivity.this.startActivity(registerIntent);
+            }
+        });
+
+        bSnack.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent registerIntent = new Intent(FoodDiaryActivity.this, AddFoodDataActivity.class);
