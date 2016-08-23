25190cd6e1c7905d00fc46b5df2eb6dba5d41735
Joelle Lim <joellelim94@gmail.com>
2016-08-23 13:49:24 +0800

    no message

--- MyApplication/app/src/main/java/sg/edu/nus/myapplication/FoodDiaryActivity.java
+++ MyApplication/app/src/main/java/sg/edu/nus/myapplication/FoodDiaryActivity.java
@@ -5,6 +5,9 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 /**
  * Created by jo on 16/8/16.
@@ -16,11 +19,20 @@ public class FoodDiaryActivity extends Activity {
         setContentView(R.layout.activity_food_diary);
 
         final Button bBreakfast = (Button) findViewById(R.id.bBreakfast);
+        final TextView tvFoodDate = (TextView) findViewById(R.id.tvFoodDate);
 
         bBreakfast.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent registerIntent = new Intent(FoodDiaryActivity.this, AddFoodDataActivity.class);
+                FoodDiaryActivity.this.startActivity(registerIntent);
+            }
+        });
+
+        tvFoodDate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent registerIntent = new Intent(FoodDiaryActivity.this, CalendarActivity.class);
                 FoodDiaryActivity.this.startActivity(registerIntent);
             }
         });
